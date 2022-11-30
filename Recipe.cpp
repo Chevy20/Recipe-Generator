9d768856eac0d13111f5546a3ca90b91174a18ee
Nicholas Baesso <nbaesso@uwo.ca>
2022-11-30 01:58:35 +0000

    Recipe.cpp edited online with Bitbucket

--- Recipe.cpp
+++ Recipe.cpp
@@ -3,10 +3,25 @@
 #include "Recipe.h"
 using namespace std;
 
+
+/**
+ * Constructor - Sets all the variables for this class.
+ */
 Recipe::Recipe()
 {
     //default     
 }
+
+
+/**
+ * Constructor - Sets all the variables for this class.
+ * 
+ * @param rname The name of the recipe.
+ * @param ingredients The list of ingredients required for the recipe.
+ * @param missingingredients List of any ingredients that are missing from the stock.
+ * @param nutri The nutrition of the recipe.
+ * @param img The image source for the picture of the recipe.
+ */
 Recipe::Recipe(std::string rname, std::vector<RecipeItem> ingredients,std::vector<RecipeItem> missingingredients, std::map<std::string, float> nutri, std::string img)
 {
     recipeName = rname;
@@ -17,35 +32,70 @@ Recipe::Recipe(std::string rname, std::vector<RecipeItem> ingredients,std::vecto
    
     
 }
+
+
+/**
+ * Destructor
+ */
 Recipe::~Recipe()
 {
 
 }
+
+
+/**
+ * Returns a list of the missing ingredients required for the recipe.
+ * 
+ * @return The list of missing ingredients.
+ */
 vector<RecipeItem> Recipe::getMissingIngredients(){
     return missingIngredients;
 }
+
+
+/**
+ * Returns the url of the image of the recipe.
+ * 
+ * @return The string of the image source.
+ */
 string Recipe::getImageURL(){
     return imageURL;
 }
+
+
+/**
+ * Returns the name of the recipe.
+ * 
+ * @return The string of the recipe name.
+ */
 string Recipe::getRecipeName()
 {
     return recipeName;
 }
+
+
+/**
+ * Returns the list of the ingredients for the recipe.
+ * 
+ * @return The ingredient list for the recipe.
+ */
 vector<RecipeItem> Recipe::getIngredients()
 {
     return recipeIngredients;
 }
+
+
+/**
+ * Returns the nutrition of the recipe.
+ * 
+ * @return The recipe's nutrition.
+ */
 map<string, float> Recipe::getNutrition()
 {
     return recipeNutrition;
 }
 
-/*
- * Function: changeProportions 
- * Description: updates all RecipeItems to create the quantity given
- * Parameters: float newYield : the new yield of the recipe after all changes made
- * Return: nothing
- */
+
 /*
  * Function: swapIngredient 
  * Description: updates all RecipeItems to create the quantity given
@@ -58,6 +108,8 @@ map<string, float> Recipe::getNutrition()
     rItem.setQuantity(nQuan);
     rItem.setItemMeasureUnit(nMeasure);
 }
+
+
 /*
  * Function: addIngredient 
  * Description: add RecipeItem to vector of ingredients given
@@ -80,6 +132,8 @@ int Recipe::addIngredient(RecipeItem nItem)
         return 0;
     
 }
+
+
 /*
  * Function: adjustIngredientAmount 
  * Description: updates a RecipeItem to the quantity given
@@ -94,8 +148,6 @@ int Recipe::adjustIngredientAmount(RecipeItem item, float quantity)
             return 0;
         }
     }
-
-  
 
     return -1;
 }
